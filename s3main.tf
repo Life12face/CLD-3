810e33f1612f84fc6bf103f625e1d6c4e73de40e
Sekou Dukuly <sdukuly00@gmail.com>
2023-10-01 13:03:41 -0400

    Module commits

--- s3main.tf
+++ s3main.tf
@@ -1,45 +1,54 @@
-resource "aws_s3_bucket" "backend" {
-  bucket = "bootcamp32-${lower(var.env)}-${random_integer.backend.result}"
+
+# Variables
+variable "bucket_name_prefix" {
+  description = "Prefix for the S3 bucket name"
+  default     = "bootcamp32"
+}
+
+variable "environment" {
+  description = "Environment (e.g., Dev, Prod)"
+  default     = "Dev"
+}
+
+variable "enable_versioning" {
+  description = "Enable S3 bucket versioning (true/false)"
+  default     = true
+}
+
+# S3 Bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "${var.bucket_name_prefix}-${var.environment}"
+  acl    = "private"
+
   tags = {
     Name        = "Sekou backend"
-    Environment = "Dev"
+    Environment = var.environment
   }
 }
 
-#5. KMS for bucket encryption
-resource "aws_kms_key" "sekoukey" {
+# KMS Key
+resource "aws_kms_key" "my_key" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
 }
 
-#6. Bucket encryption
-resource "aws_s3_bucket_server_side_encryption_configuration" "backend" {
-  bucket = aws_s3_bucket.backend.id
+# Bucket Encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_config" {
+  bucket = aws_s3_bucket.my_bucket.id
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.sekoukey.arn
+      kms_master_key_id = aws_kms_key.my_key.arn
       sse_algorithm     = "aws:kms"
     }
   }
 }
 
-# Random integer for bucket naming convention
-resource "random_integer" "backend" {
-  min = 1
-  max = 100
-  keepers = {
-    Environment = var.env
-  }
+# S3 Bucket Versioning
+resource "aws_s3_bucket_versioning" "versioning_config" {
+  bucket = aws_s3_bucket.my_bucket.id
+  enabled = var.enable_versioning
 }
-
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.backend.id
-  versioning_configuration {
-    status = var.versioning
-  }
-}
-
 
 
 
